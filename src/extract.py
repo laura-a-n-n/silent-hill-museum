a63d1a2c1e7e010095afd1422f109ab615d7035d
Laura Ann <laura@lauraann.dev>
2024-06-29 05:54:03 -0700

    renaming/formatting

--- src/extract.py
+++ src/extract.py
@@ -46,10 +46,10 @@ def process_file(filename: str):
 
         # Read index buffer data
         file.seek(triangle_index_start)
-        triangle_count = (vertex_data_start - triangle_index_start) // 2
-        triangle_indices_bytes = file.read(triangle_count * 2)
-        triangle_indices = struct.unpack(f"<{triangle_count}h", triangle_indices_bytes)
-        print(f"Triangle count: {triangle_count}")
+        triangle_index_count = (vertex_data_start - triangle_index_start) // 2
+        triangle_indices_bytes = file.read(triangle_index_count * 2)
+        triangle_indices = struct.unpack(f"<{triangle_index_count}h", triangle_indices_bytes)
+        print(f"Triangle index count: {triangle_index_count}")
 
         vertices = []
         for vertex_index in range(vertex_count):
@@ -66,72 +66,77 @@ def process_file(filename: str):
             [u, v] = struct.unpack('<2f', uv_bytes)
             vertices.append((x, y, z, w, u, v))
 
-        print("Example vertices:")
-        for i, vertex in enumerate(vertices[:5]):
-            print(f"Vertex {i + 1}: X={vertex[0]}, Y={vertex[1]}, Z={vertex[2]}, W={vertex[3]}, U={vertex[4]}, V={vertex[5]}")
-        
         print("Data seems OK!")
-        return {"vertices": vertices, "triangles": triangle_indices}
+        return {'vertices': vertices, 'triangle_indices': triangle_indices}
+
 
 def write_to_obj(mdl_data, path: str):
-  vertices = mdl_data['vertices']
-  triangles = mdl_data['triangles']
-  with open(path, 'w') as f:
-      for i, v in enumerate(vertices):
-          (x, y, z, w, _, __) = v
-          f.write(f'v  {x:.4f} {y:.4f} {z:.4f}\n')
-      f.write('\n')
+    vertices = mdl_data['vertices']
+    triangle_indices = mdl_data['triangle_indices']
+    with open(path, 'w') as f:
+        for i, v in enumerate(vertices):
+            (x, y, z, w, _, __) = v
+            f.write(f'v {x:.4f} {y:.4f} {z:.4f} {w:.4f}\n')
+        f.write('\n')
 
-      for v in vertices:
-          (x, y, z, w, u, v) = v
-          f.write(f'vt {u} {v}\n')
-      f.write('\n')
+        for v in vertices:
+            (x, y, z, w, u, v) = v
+            f.write(f'vt {u} {v}\n')
+        f.write('\n')
 
-      # triangle strip
-      true_face_count = 0
-      for i in range(2, len(triangles)):
-          v0, v1, v2 = triangles[i-2] + 1, triangles[i-1] + 1, triangles[i] + 1
-          if v0 == v1 or v1 == v2 or v0 == v2:
-              # remove degenerate triangles
-              continue
-          if i % 2 == 0:
-              f.write(f'f  {v0} {v1} {v2}\n')
-          else:
-              f.write(f'f  {v2} {v1} {v0}\n')
-          true_face_count += 1
-      f.write('\n')
-      print(f"Face count: {true_face_count}")
+        # triangle strip
+        true_face_count = 0
+        for i in range(2, len(triangle_indices)):
+            v0, v1, v2 = triangle_indices[i-2] + \
+                1, triangle_indices[i-1] + 1, triangle_indices[i] + 1
+            if v0 == v1 or v1 == v2 or v0 == v2:
+                # remove degenerate triangles
+                continue
+            if i % 2 == 0:
+                f.write(f'f {v0} {v1} {v2}\n')
+            else:
+                f.write(f'f {v2} {v1} {v0}\n')
+            true_face_count += 1
+        f.write('\n')
+        print(f"Face count: {true_face_count}")
+
 
 def validate_args(args):
     filename = args.filename
-    
+
     # Check if file exists
     if not os.path.isfile(filename):
-        raise argparse.ArgumentTypeError(f"{filename} does not exist or is not a valid file.")
-    
+        raise argparse.ArgumentTypeError(
+            f"{filename} does not exist or is not a valid file.")
+
     # Check file extension
     if not filename.lower().endswith('.mdl'):
-        raise argparse.ArgumentTypeError(f"{filename} should have a .mdl extension.")
+        raise argparse.ArgumentTypeError(
+            f"{filename} should have a .mdl extension.")
+
 
 def main():
     parser = argparse.ArgumentParser(description='...')
     parser.add_argument('filename', help='The main file to process')
-    
+
     args = parser.parse_args()
-    
+
     # Validate arguments
     try:
         validate_args(args)
     except argparse.ArgumentTypeError as e:
         parser.error(e)
-    
-    filename_base, extension = os.path.splitext(os.path.basename(args.filename))
+
+    filename_base, extension = os.path.splitext(
+        os.path.basename(args.filename))
     output_filename = f"output/{filename_base}.obj"
 
     # Print and use the new filename
     print(f"Reading file: {args.filename}")
     mdl_data = process_file(args.filename)
     write_to_obj(mdl_data, output_filename)
+    print(f"Wrote model data to {output_filename}")
+
 
 if __name__ == "__main__":
     main()
